d3d6e5a377eec96ca402cafddf2f73d4cd88f142
Bhavin8289 <bupadhyay277@gmail.com>
2023-08-14 23:57:39 +0530

    App constant

---
+++ lib/utils/constant/string_constants.dart
@@ -0,0 +1,26 @@
+class StringsConsts {
+  // app name.
+  static const String appName = 'WeMessenger';
+
+  // firestore
+  static const String usersCollection = 'Users';
+  static const String groupsCollection = 'Groups';
+  static const String statusCollection = 'Status';
+  static const String chatsCollection = 'Chats';
+  static const String callsCollection = 'Calls';
+  static const String messagesCollection = 'Messages';
+
+  // chat
+  static const String userId = 'userId';
+  static const String username = 'username';
+  static const String profilePic = 'profilePic';
+  static const String isGroupChat = 'isGroupChat';
+
+  // GIF
+  static const String giphyApiKey = 'XNWLf5zhEEFFuOpXcj61QtnliI4pYH3p';
+  static const String staticGiphyUrlStart = 'https://i.giphy.com/media/';
+  static const String staticGiphyUrlEnd = '/200.gif';
+
+  // Audio Player
+  static const String audiosSavingPath = '/flutter_sound.aac';
+}
